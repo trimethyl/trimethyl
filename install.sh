a420e6ba62121752e3929b79ebda19c37e7cb17d
Flavio De Stefano <destefano.flavio@gmail.com>
2014-09-06 17:48:07 +0200

    Shell script

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
-#!/bin/sh
-if [ -f tiapp.xml ]; then
-	wget https://github.com/CaffeinaLab/Trimethyl/archive/1.2.3.tar.gz -O T.tar.gz
-	mkdir -p app/lib/T
-	tar -xvf T.tar.gz -C app/lib/T --strip-components=1
-	ln -s ../../../lib/T/assets app/assets/iphone/images/T
-	ln -s T/alloy app/lib/alloy
-else; echo "Please move to your Titanium project to install Trimethyl"; fi
+if [ ! -f tiapp.xml ]; then
+	echo "This is not a valid Titanium project."
+	exit
+fi
+
+echo "Downloading Trimethyl.."
+curl https://github.com/CaffeinaLab/Trimethyl/archive/1.2.3.tar.gz -O T.tar.gz
+
+echo "Unzipping Trimethyl.."
+mkdir -p app/lib/T
+tar -xvf T.tar.gz -C app/lib/T --strip-components=1
+
+echo "Symlinking.."
+ln -s ../../../lib/T/assets app/assets/iphone/images/T
+ln -s T/alloy app/lib/alloy
