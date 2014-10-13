4ef86c020009907bcb91d4a4a850ff9b7d1325d3
Flavio De Stefano <flavio.destefano@caffeinalab.com>
2014-10-13 15:56:17 +0200

    Fixes install script
    Signed-off-by: Flavio De Stefano <flavio.destefano@caffeinalab.com>

---
+++ _install.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var path = require('path');
+
+var base = path.dirname(path.dirname(process.cwd()));
+console.log(base);
+
+if (!fs.existsSync( path.join(base, 'tiapp.xml') )) {
+	process.stderr.write("Not a valid Titanium project\n");
+	process.exit(1);
+}
+
+if (!fs.existsSync( path.join(base, 'app') )) {
+	process.stderr.write("Not a valid Alloy project\n");
+	process.exit(1);
+}
+
+if (!fs.existsSync( path.join(base, 'app', 'lib') )) fs.mkdirSync( path.join(base, 'app', 'lib') );
+fs.symlinkSync('../../node_modules/trimethyl',   path.join(base, 'app', 'lib', 'T')  );
