da94d8cf40867827b7b57a733b8bce133e9a656d
Flavio De Stefano <flavio.destefano@caffeinalab.com>
2015-02-04 11:06:21 +0100

    Added documentation script

---
+++ _doc.sh
@@ -0,0 +1,8 @@
+rm -rf /tmp/trimethyl
+mkdir -p /tmp/trimethyl
+
+jsduck . --output /tmp/trimethyl --title "Trimethyl API Documentation" --footer "Alloy+Titanium toolchain with superpowers" --exclude ext &&
+cd /tmp/trimethyl && git init && git checkout -b gh-pages &&
+git add -A && git commit -am "Documentation of $(date)" &&
+git remote add origin git@github.com:CaffeinaLab/Trimethyl.git &&
+git push -u -f origin gh-pages
