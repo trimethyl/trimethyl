b0d1aa366d3cab768f8f0da8db27d8e280ac471f
Flavio De Stefano <destefano.flavio@gmail.com>
2015-02-19 21:15:19 +0100

    Some optimizations and naming conventions

--- filesystem.js
+++ filesystem.js
@@ -1,57 +1,43 @@
-/* 
+/*
 * @class   Filesystem
 * @author  Andrea Jonus <andrea.jonus@caffeinalab.com>
 */
 
 /**
- * @method listDir
+ * @method listDirectory
  * Lists the content of a directory.
- * Returns a list of objects with the structure {path: "", content: []}
+ * Returns a list of objects with the structure `{path: "", content: []}`
  *
  * Returns null if the specified path does not point to an existing directory.
  *
  * @param {String}	the path of the directory
  */
-exports.listDir = function(path) {
+exports.listDirectory = function(path) {
 	var file = Ti.Filesystem.getFile(path);
-	if (file.isDirectory()) {
-		var dirArray = file.getDirectoryListing();
-		var dirList = [];
-
-		_.each(dirArray, function(item, index) {
-			var curFile = Ti.Filesystem.getFile(item);
-			dirList.push({
-				path: curFile.getNativePath(),
-				content:[]
-			});
-		});
-
-		return dirList;
-
-	} else {
+	if (!file.isDirectory()) {
 		return null;
 	}
 
-	return null;
+	return _.map(file.getDirectoryListing(), function(item) {
+		return {
+			path: Ti.Filesystem.getFile(item).getNativePath(),
+			content:[]
+		};
+	});
 };
 
-function recDesc(file) {
-	var dirArray = file.getDirectoryListing();
-	var dirList = [];
-
-	_.each(dirArray, function(item, index) {
+function recursiveDesc(file) {
+	return _.map(file.getDirectoryListing(), function(item) {
 		var curFile = Ti.Filesystem.getFile(item);
-		dirList.push({
+		return {
 			path: curFile.getNativePath(),
-			content: curFile.isDirectory() ? recDesc(curFile) : []
-		});
+			content: curFile.isDirectory() ? recursiveDesc(curFile) : []
+		};
 	});
-
-	return dirList;
 }
 
 /**
- * @method listDir
+ * @method listDirectory
  * Lists the content of a directory and all its subdirectories.
  * Returns a list of objects with the structure {path: "", content: []}
  *
@@ -59,11 +45,11 @@ function recDesc(file) {
  *
  * @param {String}	the path of the directory
  */
-exports.listDirRecursive = function(path) {
+exports.listDirectoryRecursive = function(path) {
 	var file = Ti.Filesystem.getFile(path);
-	if (file.isDirectory()) {
-		return recDesc(file);
-	} else {
+	if (!file.isDirectory()) {
 		return null;
 	}
+
+	return recursiveDesc(file);
 };
