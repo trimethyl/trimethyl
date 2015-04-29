e559d13016f5bcb8ae12e70e1ed66a5def52b7cd
Flavio De Stefano <flavio.destefano@caffeinalab.com>
2015-04-29 12:50:06 +0200

    Gendoc.sh

---
+++ gendoc.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+rm -rf /tmp/trimethyl
+mkdir -p /tmp/trimethyl
+
+jsduck framework --output /tmp/trimethyl --title "Trimethyl API Documentation" --footer "Alloy+Titanium toolchain with superpowers" --exclude framework/ext &&
+
+open /tmp/trimethyl/index.html &&
+cd /tmp/trimethyl &&
+
+git init && git checkout -b gh-pages &&
+git add -A && git commit -am "Documentation of $(date)" &&
+git remote add origin git@github.com:CaffeinaLab/Trimethyl.git &&
+git push -u -f origin gh-pages
