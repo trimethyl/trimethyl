8c717857caad938710d9a87a896cf2788e47a47e
Flavio De Stefano <flavio.destefano@caffeinalab.com>
2015-05-20 15:04:24 +0200

    Sqlite support basic queryBuilder

--- framework/sqlite.js
+++ framework/sqlite.js
@@ -13,12 +13,165 @@ exports.config = _.extend({
 
 
 function SQLite(name, file) {
+	this.query = null;
 	if (file == null) {
 		this.db = Ti.Database.open(name);
 	} else {
 		this.db = Ti.Database.install(file, name);
 	}
 }
+
+/**
+ * @method table
+ * Start a chain to query informations
+ * @param  {String} name The table name
+ * @return {SQLite}
+ */
+SQLite.prototype.table = function(name) {
+	this.query = {
+		method: '',
+		table: name,
+		where: [],
+		whereData: [],
+		select: null,
+		update: null,
+		updateData: []
+	};
+	return this;
+};
+
+/**
+ * @method select
+ * Select attributes
+ * @return {SQLite}
+ */
+SQLite.prototype.select = function() {
+	if (this.query === null) throw new Error('Start a query chain with .table() method');
+
+	this.query.method = 'select';
+
+	var args = _.toArray(arguments);
+	if (args.length > 1) {
+		this.query.select = _.object(args, args);
+	} else {
+		if (_.isArray(args[0])) this.query.select = _.object(args[0], args[0]);
+		else if (_.isObject(args[0])) this.query.select = args[0];
+		else this.query.select = _.object([args[0]], [args[0]]);
+	}
+
+	return this;
+};
+
+/**
+ * @method update
+ * Update attributes.
+ * @param  {Object} obj
+ * @return {SQLite}
+ */
+SQLite.prototype.update = function(obj) {
+	if (this.query === null) throw new Error('Start a query chain with .table() method');
+
+	this.query.method = 'update';
+	this.query.update = _.keys(obj);
+	this.query.updateData = _.values(obj);
+
+	return this;
+};
+
+/**
+ * @method delete
+ * Perform a delete table.
+ * @return {SQLite}
+ */
+SQLite.prototype.delete = function() {
+	if (this.query === null) throw new Error('Start a query chain with .table() method');
+
+	this.query.method = 'delete';
+
+	return this;
+};
+
+/**
+ * @method truncate
+ * Perform a truncate table.
+ * @return {[SQLite}
+ */
+SQLite.prototype.truncate = function() {
+	if (this.query === null) throw new Error('Start a query chain with .table() method');
+
+	this.query.method = 'truncate';
+
+	return this;
+};
+
+/**
+ * @method andWhere
+ * Add where clauses.
+ * @return {SQLite}
+ */
+SQLite.prototype.andWhere = function() {
+	if (this.query === null) throw new Error('Start a query chain with .table() method');
+
+	var args = _.toArray(arguments);
+	if (args.length === 1) {
+		this.query.where.push(args[0]);
+	} else if (args.length === 2) {
+		this.query.where.push(args[0] + ' = ?');
+		this.query.whereData.push(args[1]);
+	} else if (args.length === 3) {
+		this.query.where.push(args[0] + ' ' + args[1] + ' ?');
+		this.query.whereData.push(args[2]);
+	}
+
+	return this;
+};
+
+/**
+ * @method getExequery
+ * @return {[type]} [description]
+ */
+SQLite.prototype.getExequery = function() {
+	if (this.query === null) throw new Error('Start a query chain with .table() method');
+
+	var whereClause = (this.query.where.length > 0 ? (' WHERE ' + this.query.where.join(' AND ')) : '');
+	switch (this.query.method) {
+
+		case 'select':
+		return [
+			'SELECT ' + (this.query.select === null ? '*' : _.map(this.query.select, function(v, k){ return k + ' AS ' + v; }).join(',')) +
+			' FROM ' + this.query.table +
+			whereClause
+		]
+		.concat(this.query.whereData);
+		break;
+
+		case 'update':
+		return [
+			'UPDATE ' + this.query.table +
+			' SET ' + (this.query.update.join(' = ?, ') + ' = ?') +
+			whereClause
+		]
+		.concat(this.query.updateData)
+		.concat(this.query.whereData);
+		break;
+
+		case 'delete':
+		return [
+			'DELETE FROM ' + this.query.table +
+			whereClause
+		]
+		.concat(this.query.whereData);
+		break;
+
+		case 'truncate':
+		return [
+			'TRUNCATE TABLE ' + this.query.table
+		];
+		break;
+
+	}
+};
+
 
 /**
  * @method close
@@ -40,11 +193,15 @@ SQLite.prototype.close = function() {
  * @return {Ti.DB.ResultSet}
  */
 SQLite.prototype.execute = SQLite.prototype.exec = function() {
-	if (exports.config.log) {
-		Ti.API.debug('SQLite:', arguments);
+	if (this.query === null) {
+		if (exports.config.log) Ti.API.debug('SQLite:', arguments);
+		return Function.prototype.apply.call(this.db.execute, this.db, arguments);
 	}
 
-	return Function.prototype.apply.call(this.db.execute, this.db, arguments);
+	var q = this.getExequery();
+	this.query = null; // Reset query
+	if (exports.config.log) Ti.API.debug('SQLite:', q);
+	return Function.prototype.apply.call(this.db.execute, this.db, q);
 };
 
 /**
@@ -56,6 +213,7 @@ SQLite.prototype.execute = SQLite.prototype.exec = function() {
 SQLite.prototype.value = SQLite.prototype.val = function() {
 	var row = this.execute.apply(this, arguments);
 	if (row.validRow === false) return null;
+
 	return row.field(0);
 };
 
@@ -68,6 +226,7 @@ SQLite.prototype.value = SQLite.prototype.val = function() {
 SQLite.prototype.single = SQLite.prototype.row = function() {
 	var row = this.execute.apply(this, arguments);
 	if (row.validRow === false) return null;
+
 	var obj = {};
 	for (var i = 0; i < row.fieldCount; i++) {
 		obj[row.fieldName(i)] = row.field(i);
