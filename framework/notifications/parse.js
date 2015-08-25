3f6cfe246bb0addfbd4c69734d51a3c8b162a078
Flavio De Stefano <flavio.destefano@caffeinalab.com>
2015-08-25 10:13:52 +0200

    Introduced app base in parse

--- framework/notifications/parse.js
+++ framework/notifications/parse.js
@@ -9,6 +9,12 @@ var app_id = Ti.App.Properties.getString('parse.appid');
 var rest_api_key = Ti.App.Properties.getString('parse.restapikey');
 var gcm_sender_id = Ti.App.Properties.getString('gcm.senderid');
 
+var app_base = {
+	appName: Ti.App.name,
+	appVersion: Ti.App.version,
+	appIdentifier: Ti.App.id
+};
+
 exports.subscribe = function(opt) {
 	if (app_id == null) throw new Error("Notifications.Parse: Invalid Parse application ID. Set it in your tiapp.xml");
 	if (rest_api_key == null) throw new Error("Notifications.Parse: Invalid REST API key. Set it in your tiapp.xml");
@@ -26,19 +32,19 @@ exports.subscribe = function(opt) {
 		},
 		data: JSON.stringify((function() {
 			if (OS_IOS) {
-				return {
+				return _.extend({
 					deviceType: 'ios',
 					deviceToken: opt.deviceToken,
 					channels: [ opt.channel || "" ],
-				};
+				}, app_base);
 			} else if (OS_ANDROID) {
-				return {
+				return _.extend({
 					deviceType: 'android',
 					pushType: 'gcm',
 					deviceToken: opt.deviceToken,
 					GCMSenderId: gcm_sender_id,
 					channels: [ opt.channel || "" ],
-				};
+				}, app_base);
 			}
 		})()),
 		success: opt.success,
@@ -65,19 +71,19 @@ exports.unsubscribe = function(opt) {
 		},
 		data: JSON.stringify((function() {
 			if (OS_IOS) {
-				return {
+				return _.extend({
 					deviceType: 'ios',
 					deviceToken: opt.deviceToken,
 					channels: [ opt.channel || "" ],
-				};
+				}, app_base);
 			} else if (OS_ANDROID) {
-				return {
+				return _.extend({
 					deviceType: 'android',
 					pushType: 'gcm',
 					deviceToken: opt.deviceToken,
 					GCMSenderId: gcm_sender_id,
 					channels: [ opt.channel || "" ],
-				};
+				}, app_base);
 			}
 		})()),
 		success: opt.success,
