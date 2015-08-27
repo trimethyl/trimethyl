7473fa5dd90d644b47999a4de69cc9db0ee149fb
Jei <j.jonus.j@gmail.com>
2015-08-27 10:55:07 +0200

    Fixed missing parameter bug in FB.

--- framework/fb.js
+++ framework/fb.js
@@ -6,9 +6,11 @@
 /**
  * @property config
  * @property {Array} [config.permissions=[]] Array of permissions
+ * @property {Number} [config.timeout] Request timeout
  */
 exports.config = _.extend({
 	permissions: [],
+	timeout: 20000
 }, Alloy.CFG.T ? (Alloy.CFG.T.fb || Alloy.CFG.T.facebook) : {});
 
 var Util = require('T/util');
@@ -20,11 +22,11 @@ if (Facebook) {
 	Facebook.appid = Ti.App.Properties.getString('ti.facebook.appid');
 
 	if (!_.isEmpty(exports.config.permissions)) {
-		Facebook.setPermissions(exports.config.permissions);
+		Facebook.permissions = exports.config.permissions;
 	}
 
 	if (_.isFunction(Facebook.initialize)) {
-		Facebook.initialize();
+		Facebook.initialize(+exports.config.timeout);
 	}
 
 }
