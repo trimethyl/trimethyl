7e295f4185832da8a42fcc376ade9a86a3f6f616
Flavio De Stefano <destefano.flavio@gmail.com>
2015-11-15 23:01:30 +0100

    New tools

---
+++ publisher.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+	echo "Invalid argument"
+	exit
+fi
+
+npm run test
+
+if [ $? -eq 0 ]; then
+	npm version $1 && npm publish && git push && git push --tags
+	npm run gendoc
+fi
