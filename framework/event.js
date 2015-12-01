507c6dc227fcf6c115d61dec24480385edf1d544
Flavio De Stefano <flavio.destefano@caffeina.it>
2015-12-01 14:49:16 +0100

    New events feature

--- framework/event.js
+++ framework/event.js
@@ -3,4 +3,20 @@
  * @author  Flavio De Stefano <flavio.destefano@caffeinalab.com>
  */
 
-module.exports = _.extend({}, Backbone.Events);
+var Event = _.extend({
+
+	onWithObject: function(obj) {
+		_.each(obj, function(fn, k) {
+			Event.on(k, fn);
+		});
+	},
+
+	offWithObject: function(obj) {
+		_.each(obj, function(fn, k) {
+			Event.off(k, fn);
+		});
+	}
+
+}, Backbone.Events);
+
+module.exports = Event;
