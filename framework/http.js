0fa6aa10519ad92aac5d57cec1759e9c7984ff20
Flavio De Stefano <flavio.destefano@caffeina.it>
2016-01-25 14:58:31 +0100

    little code refactor

--- framework/http.js
+++ framework/http.js
@@ -598,13 +598,17 @@ exports.download = function(url, file, success, error, ondatastream) {
 		});
 	};
 
-	if (OS_IOS) doDownload();
-	else {
+	if (OS_IOS) {
+		doDownload();
+	} else if (OS_ANDROID) {
 		Ti.Media.requestCameraPermissions(function(res) {
-			if (res.success === true) doDownload();
-			else error({
-				message: L('missing_permission_write_file', 'Missing permission to write file')
-			});
+			if (res.success === false) {
+				return error({
+					message: L('missing_permission_write_file', 'Missing permission to write file')
+				});
+			}
+
+			doDownload();
 		});
 	}
 };
