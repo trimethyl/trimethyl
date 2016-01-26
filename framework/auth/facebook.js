24c825e342d5a0871889c6505ca84c5bcb073ad4
Flavio De Stefano <flavio.destefano@caffeina.com>
2016-01-26 17:59:18 +0100

    New way to handle login FB events

--- framework/auth/facebook.js
+++ framework/auth/facebook.js
@@ -14,34 +14,14 @@ exports.config = _.extend({
 var Util = require('T/util');
 var _FB = require('T/fb'); // Use FB as an accessor
 
+var localOptions = null;
+
 function storeData() {
 	Ti.App.Properties.setObject('auth.facebook.data', {
 		accessToken: _FB.accessToken,
 		expirationDate: _FB.expirationDate
 	});
 }
-
-if (OS_IOS) {
-	// It seems that iOS (sometimes) doesn't trigger the login event
-	// This is very bad, and this below is an hack, but it works:
-	// We listen for the resumed event (it only happens when the Facebook.app is opened),
-	// and in a defer call (with this trick we can assume that,
-	// if in a future update Facebook will fix this issue,
-	// its login event will be triggered before our function)
-	// we re-fire the event with the minimal data we have
-	Ti.App.addEventListener('resumed', function() { _.defer(function() {
-		var currentSchema = Util.parseSchema();
-		if (/^fb\d+\:\/\/authorize/.test(currentSchema)) {
-			_FB.fireEvent('login', {
-				manualFire: true,
-				success: _FB.loggedIn,
-				accessToken: _FB.accessToken
-			});
-		}
-	}); });
-}
-
-var localOptions = null;
 
 exports.login = function(opt) {
 	localOptions = opt; // store globally
@@ -73,31 +53,35 @@ exports.storedLogin = function(opt) {
 Init
 */
 
-_FB.addEventListener('login', function(e) {
+_FB.addLoginListener(function(e) {
 	Ti.API.debug('Auth.Facebook: login fired', e);
 
 	if (e.success) {
 		storeData();
+	} else {
+		// If there's some errors, reset
+		exports.logout();
 	}
 
 	// This is a security hack caused by iOS SDK that automatically trigger the login event
 	// We don't need that is event is triggered on startup: to detect login,
 	// just call `Auth.login({ driver: 'facebook' })`
 	if (localOptions == null) {
-		return Ti.API.warn('Auth.Facebook: login prevented');
-	}
-
-	if (e.success) {
-		localOptions.success({
-			access_token: _FB.accessToken
-		});
+		Ti.API.warn('Auth.Facebook: login prevented');
 	} else {
-		_FB.logout();
-		localOptions.error({
-			message: (e.error && e.error.indexOf('OTHER:') !== 0) ? e.error : L('unexpected_error', 'Unexpected error')
-		});
-	}
 
-	// Reset localOptions to prevent double triggers of callbacks
-	localOptions = null;
+		if (e.success) {
+			localOptions.success({
+				access_token: _FB.accessToken
+			});
+		} else {
+			localOptions.error({
+				message: (e.error && e.error.indexOf('OTHER:') !== 0) ? e.error : L('unexpected_error', 'Unexpected error')
+			});
+		}
+
+		// Reset localOptions to prevent double triggers of callbacks
+		localOptions = null;
+
+	}
 });
--- framework/fb.js
+++ framework/fb.js
@@ -14,10 +14,16 @@ exports.config = _.extend({
 var Util = require('T/util');
 var _FB = Util.requireOrNull('facebook');
 
+var dispatcher = _.extend({}, Backbone.Events);
+
 if (_FB) {
 
 	// Hydratate module
 
+	/**
+	 * @method fetchUser
+	 * @param  {Object} opt
+	 */
 	_FB.fetchUser = function(opt) {
 		opt = _.defaults(opt || {}, {
 			fields: 'name,email,first_name,last_name'
@@ -32,13 +38,59 @@ if (_FB) {
 		});
 	};
 
+	// Setup permissions
 	if (!_.isEmpty(exports.config.permissions)) {
 		_FB.permissions = exports.config.permissions;
 	}
 
-	if (_.isFunction(_FB.initialize)) {
-		_FB.initialize();
-	}
+	// Setup login events and forward to internal JS dispatcher
+	
+	/**
+	 * @method addLoginListener
+	 * @param {Function} callback)
+	 */
+	_FB.addLoginListener = function(callback) {
+		dispatcher.on('login', callback); 
+	};
+
+	/**
+	 * @method removeLoginListener
+	 * @param  {Function} callback
+	 */
+	_FB.removeLoginListener = function(callback) { 
+		dispatcher.off('login', callback); 
+	};
+
+	/**
+	 * @method addLogoutListener
+	 * @param {Function} callback)
+	 */
+	_FB.addLogoutListener = function(callback) {
+		dispatcher.on('logout', callback); 
+	};
+
+	/**
+	 * @method removeLogoutListener
+	 * @param  {Function} callback
+	 */
+	_FB.removeLogoutListener = function(callback) { 
+		dispatcher.off('logout', callback); 
+	};
+
+	// Forward
+	_FB.addEventListener('login', function(e) { 
+		Ti.API.debug('FB: login fired', e);
+		dispatcher.trigger('login', e); 
+	});
+
+	_FB.addEventListener('logout', function(e) { 
+		Ti.API.debug('FB: logout fired', e);
+		dispatcher.trigger('logout', e); 
+	});
+
+
+	// Initialize the SDK and login events
+	_FB.initialize();
 
 } else {
 
