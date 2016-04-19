9610ab0f233c3a1a46ffb4643734ee0b59aaa069
Flavio De Stefano <flavio.destefano@caffeina.com>
2016-04-19 16:48:24 +0200

    Added docset tgz

--- gendoc.sh
+++ gendoc.sh
@@ -4,8 +4,11 @@ rm -rf /tmp/trimethyl-docset; mkdir -p /tmp/trimethyl-docset
 rm -rf /tmp/trimethyl-wiki; mkdir -p /tmp/trimethyl-wiki
 
 cp -R guides /tmp/trimethyl-wiki &&
+
 jsdoc -c jsdoc.conf.json -d /tmp/trimethyl-docset &&
+
 jsdoc -c jsdoc.conf.json -d docset -p -t node_modules/jsdoc-dash-template &&
+tar --exclude='.DS_Store' -cvzf docset/Trimethyl.tgz docset/Trimethyl.docset &&
 
 cd /tmp/trimethyl-docset &&
 git init && git checkout -b gh-pages &&
