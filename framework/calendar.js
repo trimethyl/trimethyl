853aafbe5f5b025e89dc5890f1a2312f95cde6b9
Flavio De Stefano <flavio.destefano@caffeina.com>
2016-06-03 17:04:51 +0200

    Now calendar uses Rrule

--- framework/calendar.js
+++ framework/calendar.js
@@ -13,90 +13,130 @@ var Util = require('T/util');
 var Moment = require('alloy/moment');
 var Permissions = require('T/permissions');
 
+var RRule = require('T/ext/rrule');
+exports.RRule = RRule;
+
 if (OS_ANDROID) {
 	var LDACalendar = require('lucadamico.android.calendar');
 }
 
-var BYDAY = {
-	'SU' : 1,
-	'MO' : 2,
-	'TU' : 3,
-	'WE' : 4,
-	'TH' : 5,
-	'FR' : 6,
-	'SA' : 7
+var RRT = {
+	map: {
+		freq: {
+			RR_to_IOS: {},
+			IOS_to_RR: {}
+		},
+		weekday: {
+			RR_to_IOS: {},
+			IOS_to_RR: {}
+		},
+	},
+	props: {
+		RR_to_IOS: {},
+		IOS_to_RR: {}
+	}
 };
 
-/*
-http://www.kanzaki.com/docs/ical/recur.html
-http://docs.oracle.com/cd/B25221_04/web.1013/b15876/rfc-2445.htm
-*/
+exports.RRT = RRT;
 
-var RRules = {
+RRT.map.freq.RR_to_IOS[ JSON.stringify(RRule.YEARLY) ] = Ti.Calendar.RECURRENCEFREQUENCY_YEARLY;
+RRT.map.freq.RR_to_IOS[ JSON.stringify(RRule.MONTHLY) ] = Ti.Calendar.RECURRENCEFREQUENCY_MONTHLY;
+RRT.map.freq.RR_to_IOS[ JSON.stringify(RRule.WEEKLY) ] = Ti.Calendar.RECURRENCEFREQUENCY_WEEKLY;
+RRT.map.freq.RR_to_IOS[ JSON.stringify(RRule.DAILY) ] = Ti.Calendar.RECURRENCEFREQUENCY_DAILY;
 
-	build: function(startDate, r) {
-		var startDateStr = Moment(startDate).format('YYYYMMDDTHHmmss');
+RRT.map.weekday.RR_to_IOS[ JSON.stringify(RRule.MO) ] = 2;
+RRT.map.weekday.RR_to_IOS[ JSON.stringify(RRule.TU) ] = 3;
+RRT.map.weekday.RR_to_IOS[ JSON.stringify(RRule.WE) ] = 4;
+RRT.map.weekday.RR_to_IOS[ JSON.stringify(RRule.TH) ] = 5;
+RRT.map.weekday.RR_to_IOS[ JSON.stringify(RRule.FR) ] = 6;
+RRT.map.weekday.RR_to_IOS[ JSON.stringify(RRule.SA) ] = 7;
+RRT.map.weekday.RR_to_IOS[ JSON.stringify(RRule.SU) ] = 1;
 
-		var freq = r.frequency.toUpperCase();
+RRT.map.freq.IOS_to_RR[ JSON.stringify(Ti.Calendar.RECURRENCEFREQUENCY_YEARLY) ] = RRule.YEARLY;
+RRT.map.freq.IOS_to_RR[ JSON.stringify(Ti.Calendar.RECURRENCEFREQUENCY_MONTHLY) ] = RRule.MONTHLY;
+RRT.map.freq.IOS_to_RR[ JSON.stringify(Ti.Calendar.RECURRENCEFREQUENCY_WEEKLY) ] = RRule.WEEKLY;
+RRT.map.freq.IOS_to_RR[ JSON.stringify(Ti.Calendar.RECURRENCEFREQUENCY_DAILY) ] = RRule.DAILY;
 
-		var parts = [];
-		parts.push("FREQ=" + freq); 
-		//	parts.push("DTSTART=" + startDateStr);
+RRT.map.weekday.IOS_to_RR[ 2 ] = RRule.MO;
+RRT.map.weekday.IOS_to_RR[ 3 ] = RRule.TU;
+RRT.map.weekday.IOS_to_RR[ 4 ] = RRule.WE;
+RRT.map.weekday.IOS_to_RR[ 5 ] = RRule.TH;
+RRT.map.weekday.IOS_to_RR[ 6 ] = RRule.FR;
+RRT.map.weekday.IOS_to_RR[ 7 ] = RRule.SA;
+RRT.map.weekday.IOS_to_RR[ 1 ] = RRule.SU;
 
-		// The recurrence end can be specified by a date (date-based) or by a maximum count of occurrences (count-based)
-		// ; either UNTIL or COUNT may appear in a 'recur',
-      // ; but UNTIL and COUNT MUST NOT occur in the same 'recur'
-		if (r.end != null) {
-			if (r.end.endDate != null) {
-				parts.push("UNTIL=" + Moment(r.end.endDate).format('YYYYMMDDTHHmmss'));
-			} else if (r.end.occurrenceCount != null) {
-				parts.push("COUNT=" + Number(r.end.occurrenceCount));
-			} else {
-				throw new Error("The end property must specify endDate or occurrenceCount");
-			}
-		}
-
-		if (r.interval != null) {
-			parts.push("INTERVAL=" + Number(r.interval));
-		}
-
-		// The days of the month that the event occurs, as an array of number objects. 
-		// Values can be from 1 to 31 and from -1 to -31. 
-		if (r.daysOfTheMonth != null) {
-			if (freq != 'MONTHLY') throw new Error("daysOfTheMonth rule is only valid with MONTHLY frequency");
-			parts.push("BYMONTHDAY=" + r.daysOfTheMonth.join(','));
-		}
-
-		// The days of the year that the event occurs, as an array of number objects. 
-		// Values can be from 1 to 366 and from -1 to -366.
-		else if (r.daysOfTheYear != null) {
-			if (freq != 'YEARLY') throw new Error("daysOfTheYear rule is only valid with YEARLY frequency");
-			parts.push("BYYEARDAY=" + r.daysOfTheYear.join(','));
-		}
-
-		// The months of the year that the event occurs, as an array of Number objects. 
-		// Values can be from 1 to 12. 
-		else if (r.monthsOfTheYear != null) {
-			if (freq != 'YEARLY') throw new Error("monthsOfTheYear rule is only valid with YEARLY frequency");
-			parts.push("BYMONTH=" + r.monthsOfTheYear.join(','));
-		}
-
-		// The weeks of the year that the event occurs, as an array of number objects. 
-		// Values can be from 1 to 53 and from -1 to -53. 
-		else if (r.weeksOfTheYear != null) {
-			if (freq != 'YEARLY') throw new Error("weeksOfTheYear rule is only valid with YEARLY frequency");
-			parts.push("BYWEEKNO=" + r.weeksOfTheYear.join(','));
-		}
-
-		// The day of the week. 
-		// Values are from 1 to 7, with Sunday being 1.
-		else if (r.daysOfTheWeek != null) {
-			parts.push("BYDAY=" + r.daysOfTheWeek.join(','));
-		}
-
-		return parts.join(';');
+RRT.props.RR_to_IOS = {
+	interval : 'interval',
+	count: function(ios, value) { 
+		ios.end = { occurrenceCount: value }; 
+	},
+	until: function(ios, value) { 
+		ios.end = { endDate: value }; 
+	},
+	freq : function(ios, value) { 
+		ios.frequency = RRT.map.freq.RR_to_IOS[ JSON.stringify(value) ]; 
+	},
+	bymonth: 'monthsOfTheYear',
+	bymonthday: 'daysOfTheMonth',
+	byyearday: 'daysOfTheYear',
+	byweekday: function(ios, value) {
+		ios.daysOfTheWeek = value.map(function(day) {
+			return { dayOfWeek: RRT.map.weekday.RR_to_IOS[ JSON.stringify(day) ] };
+		});
 	}
+};
 
+RRT.props.IOS_to_RR = {
+	interval : 'interval',
+	end: function(rruleOpt, value) {
+		if (value.occurrenceCount) {
+			rruleOpt.count = value.occurrenceCount;
+		} else if (value.endDate) {
+			rruleOpt.until = value.occurrenceCount;
+		}
+	},
+	frequency: function(rruleOpt, value) { 
+		rruleOpt.freq = RRT.map.freq.IOS_to_RR[ JSON.stringify(value) ]; 
+	},
+	monthsOfTheYear: 'bymonth',
+	daysOfTheMonth: 'bymonthday',
+	daysOfTheYear: 'byyearday',
+	daysOfTheWeek: function(rruleOpt, value) {
+		rruleOpt.byweekday = value.map(function(wkDayObj) {
+			return RRT.map.weekday.IOS_to_RR[ JSON.stringify(wkDayObj.dayOfWeek) ];
+		});
+	}
+};
+
+RRT.RRuleOptToiOSRecurrenceRule = function(rruleOpt) {
+	var ios = {};
+	_.each(RRT.props.RR_to_IOS, function(ios_prop_key, rrule_prop_key) {
+		var rrule_prop_val = rruleOpt[ rrule_prop_key ];
+		if (rrule_prop_val == null) return;
+		if (_.isArray(rrule_prop_val) && rrule_prop_val.length === 0) return;
+
+		if (_.isString(ios_prop_key)) {
+			ios[ ios_prop_key ] = rrule_prop_val;
+		} else if (_.isFunction(ios_prop_key)) {
+			ios_prop_key(ios, rrule_prop_val);
+		}
+	});
+	return ios;
+};
+
+RRT.iOSRecurrenceRuleToRRuleOpt = function(ios) {
+	var rruleOpt = {};
+	_.each(RRT.props.IOS_to_RR, function(rrule_prop_key, ios_prop_key) {
+		var ios_prop_val = ios[ ios_prop_key ];
+		if (ios_prop_val == null) return;
+
+		if (_.isString(rrule_prop_key)) {
+			rruleOpt[ rrule_prop_key ] = ios_prop_val;
+		} else if (_.isFunction(rrule_prop_key)) {
+			rrule_prop_key(rruleOpt, ios_prop_val);
+		}
+	});
+	return rruleOpt;
 };
 
 /**
@@ -115,8 +155,8 @@ exports.createEvent = function(calendar, opt) {
 	if (opt.begin == null) throw new Error("Calendar: Set a begin date");
 	if (opt.end == null && opt.allDay != true) throw new Error("Calendar: Set an end date or an allDay event");
 
-	if (_.isString(opt.begin)) opt.begin = new Date(opt.begin);
-	if (_.isString(opt.end)) opt.end = new Date(opt.end);
+	if (_.isString(opt.begin)) opt.begin = Moment(opt.begin).getDate();
+	if (_.isString(opt.end)) opt.end = Moment(opt.end).getDate();
 	
 	if (opt.end < opt.begin) throw new Error("Calendar: The end date is lesser than begin date");
 
@@ -128,6 +168,10 @@ exports.createEvent = function(calendar, opt) {
 
 	if (opt.alerts != null) {
 		exports.setAlerts(event, opt.alerts);
+	}
+
+	if (OS_IOS) {
+		event.save( Ti.Calendar.SPAN_FUTUREEVENTS );
 	}
 
 	return event.id;
@@ -151,31 +195,21 @@ exports.getEventById = function(calendar, id) {
  * @param {Ti.Calendar.Event} event  The event.
  * @param {Object} rule  An object with recurrence rule.
  */
-exports.setRecurrenceRule = function(event, rule) {
-	rule.interval = rule.interval || 1;
+exports.setRecurrenceRule = function(event, rruleOpt) {
+	rruleOpt.interval = rruleOpt.interval || 1;
+	rruleOpt.dtstart = Moment(event.begin).toDate();
 
  	if (OS_IOS) {
 
- 		// Convert the special rule daysOfTheWeek to the iOS equivalent
- 		if (rule.daysOfTheWeek != null) {
- 			rule.daysOfTheWeek = rule.daysOfTheWeek.map(function(e) {
- 				return { 
- 					week: 0,
- 					dayOfWeek: BYDAY[e]
- 				};
- 			});
- 		}
-
-		rule.frequency = Ti.Calendar['RECURRENCEFREQUENCY_' + rule.frequency.toUpperCase()];
-		event.recurrenceRules = [ rule ].map(event.createRecurrenceRule);
+ 		var iosRRule = RRT.RRuleOptToiOSRecurrenceRule(rruleOpt);
+ 		event.recurrenceRules = [ event.createRecurrenceRule(iosRRule) ];
 		event.save( Ti.Calendar.SPAN_FUTUREEVENTS );
 
 	} else if (OS_ANDROID) {
 
-		var rrule = RRules.build(event.begin, rule);
-		var status = (1 == LDACalendar.updateEventRecurrenceRule(event.id, rrule));
-		if (!status) {
-			throw new Error("Calendar: errore in update event recurrence rule");
+		var rows = LDACalendar.updateEventRecurrenceRule(event.id, RRule.toString(rruleOpt));
+		if (rows != 1) {
+			throw new Error("Error while saving recurrence rules");
 		}
 
 	}
@@ -184,22 +218,21 @@ exports.setRecurrenceRule = function(event, rule) {
 };
 
 exports.getRecurrenceRule = function(event) {
-	var rule = null;
+	var rrule = null;
 
 	if (OS_IOS) {
-
 		if (event.recurrenceRules != null) {
-			rule = event.recurrenceRules[0];
-			switch (rule.frequency) {
-				case Ti.Calendar.RECURRENCEFREQUENCY_DAILY: 
-			}
+			var rruleOpt = RRT.iOSRecurrenceRuleToRRuleOpt(event.recurrenceRules[0]);
+			rruleOpt.dtstart = Moment(event.begin).toDate();
+			rrule = new RRule(rruleOpt);
 		}
-
 	} else if (OS_ANDROID) {
-
+		if (event.rrule != null) {
+			rrule = RRule.fromString(event.rrule);
+		}
 	}
 
-	return rule;
+	return rrule;
 };
 
 /**
@@ -210,26 +243,42 @@ exports.getRecurrenceRule = function(event) {
  */
 exports.setAlerts = function(event, alerts) {
 	if (OS_IOS) {
-
 		event.alerts = _.map(alerts, function(minutes) {
 			return event.createAlert({
-				relativeOffset: - (minutes * 60 * 1000)
+				relativeOffset: -1 * (minutes * 60 * 1000)
 			});
 		});
-
 		event.save( Ti.Calendar.SPAN_FUTUREEVENTS );
-
-	} else {
-		
+	} else if (OS_ANDROID) {
 		LDACalendar.deleteAllEventReminders(event.id);
 		_.each(alerts, function(minutes) {
 			var status = (1 == LDACalendar.addReminderToEvent(event.id, minutes));
-			if (!status) throw new Error("Calendar: errore in update event recurrence rule");
+			if (!status) throw new Error("Calendar: error in update event recurrence rule");
 		});
-
 	}
 	
 	return event;
+};
+
+/**
+ * Get the alerts for the event.
+ * @param  {Ti.Calendar.Event} event
+ * @return {Array} alerts
+ */
+exports.getAlerts = function(event) {
+	var alerts = [];
+
+	if (OS_IOS) {
+		alerts = event.getAlerts().map(function(a) {
+			return -1 * (a.relativeOffset / (60 * 1000));
+		});
+	} else if (OS_ANDROID) {
+		alerts = event.getAlerts().map(function(a) {
+			return a.minutes;
+		});
+	}
+	
+	return alerts;
 };
 
 /**
@@ -239,7 +288,6 @@ exports.setAlerts = function(event, alerts) {
  */
 exports.deleteEvent = function(event) {
 	if (OS_IOS) {
-
 		try {
 			event.remove( Ti.Calendar.SPAN_FUTUREEVENTS );
 			return true;
@@ -247,12 +295,13 @@ exports.deleteEvent = function(event) {
 			Ti.API.error("Calendar: failed deleting event", ex.message);
 			return false;
 		}
-
 	} else if (OS_ANDROID) {
-
-		var status = (1 == LDACalendar.deleteEvent(event.id));
-		if (!status) Ti.API.error("Calendar: failed deleting event");
-		return status;
+		try {
+			var rows = LDACalendar.deleteEvent(event.id);
+			return (rows == 1);
+		} catch (ex) {
+			return false;
+		}
 	}
 };
 
@@ -299,8 +348,8 @@ exports.getAllCalendars = function() {
  * @return {Ti.Calendar.Event[]}
  */
 exports.getEventsBetweenDates = function(cal, d1, d2) {
-	if (_.isString(d1)) d1 = new Date(d1);
-	if (_.isString(d2)) d2 = new Date(d2);
+	if (_.isString(d1)) d1 = Moment(d1).getDate();
+	if (_.isString(d2)) d2 = Moment(d2).getDate();
 
 	return cal.getEventsBetweenDates(d1, d2);
 };
