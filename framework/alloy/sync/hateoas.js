5681e561d30a47c9e4da7e8905c440cd1ccb8efe
Jei <j.jonus.j@gmail.com>
2016-07-21 15:33:48 +0200

    Added alloy/sync/hateoas

---
+++ framework/alloy/sync/hateoas.js
@@ -0,0 +1,102 @@
+/*
+ * @author  Flavio De Stefano <flavio.destefano@caffeina.com>
+ * @author  Andrea Jonus <andrea.jonus@caffeina.com>
+ */
+
+var HTTP = require('T/http');
+var Util = require('T/util');
+
+var CRUD_to_REST = {
+	'create': 'POST',
+	'read': 'GET'
+};
+
+var CONTENT = 'content';
+var ATTRIBUTES = 'attributes';
+var ERROR_TYPE = 'error';
+
+exports.sync = function(method, model, opt) {
+	var url = (model.config.adapter.baseUrl || '/') + model.config.adapter.name;
+	var query = null;
+
+	if (model instanceof Backbone.Model) {
+		url += model.id != null ? ('/' + model.id) : '';
+		query = model.get('query');
+	} else if (model.first() != null) {
+		query = model.first().get('query');
+	}
+
+	query = query || opt.query;
+
+	if (query != null) {
+		if (_.isObject(query) || _.isArray(query)) {
+			url += Util.buildQuery(query);
+		} else if (_.isString(query)) {
+			url += query.substr(0,1) === '?' ? query : ('?'+query);
+		}
+	}
+
+	var httpOpt = _.extend({}, model.config.http, opt.http, {
+		url: url,
+		method: CRUD_to_REST[method] || 'GET',
+		format: 'json'
+	});
+
+	if (Alloy.Backbone.emulateHTTP) {
+		if ([ 'DELETE', 'PUT', 'PATCH' ].indexOf(httpOpt.method) !== -1) {
+			httpOpt.method = 'POST';
+			httpOpt.headers = _.extend({}, httpOpt.headers, {
+				'X-HTTP-Method-Override': httpOpt.method
+			});
+		}
+	}
+
+	switch (method) {
+
+		case 'create':
+
+		_.extend(httpOpt, { data: JSON.stringify(model.toJSON()) });
+		HTTP.send(httpOpt).success(function(resp) {
+
+			if (resp != null && resp.id != null) {
+				opt.success(resp);
+			} else {
+				opt.error();
+			}
+
+		}).error(opt.error);
+		break;
+
+		case 'read':
+
+		HTTP.send(httpOpt).success(function(resp) {
+
+			if (resp != null && _.isObject(resp)) {
+				if (resp[ATTRIBUTES] && resp[ATTRIBUTES].type && resp[ATTRIBUTES].type == ERROR_TYPE) {
+					opt.error();
+				}
+
+				if (resp[CONTENT] != null) {
+					var content = resp[CONTENT];
+
+					if (_.isArray(content)){
+						if (model instanceof Backbone.Collection) {
+							opt.success(content);
+						} else {
+							opt.success(_.first(content));
+						}
+					} else {
+						opt.error();
+					}
+				}
+			} else {
+				opt.error();
+			}
+
+		}).error(function(err) {
+			opt.error(err);
+		});
+		break;
+
+	}
+};
