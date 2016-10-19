f53cf1b3e3db566115563176e769efd3be2fefc0
Flavio De Stefano <flavio.destefano@caffeina.com>
2016-10-19 15:36:02 +0200

    Better gendoc

--- gendoc.sh
+++ gendoc.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
-rm -rf /tmp/trimethyl-docset; mkdir -p /tmp/trimethyl-docset
-rm -rf /tmp/trimethyl-wiki; mkdir -p /tmp/trimethyl-wiki
+# Prepare directores
+rm -rf /tmp/trimethyl-gh-pages
+mkdir -p /tmp/trimethyl-gh-pages
+rm -rf /tmp/trimethyl-docset
+mkdir -p /tmp/trimethyl-docset
 
-jsdoc -c jsdoc.conf.json -d /tmp/trimethyl-docset &&
+# Generate gh-pages
+jsdoc -c jsdoc.conf.json -d /tmp/trimethyl-gh-pages &&
 
-jsdoc -c jsdoc.conf.json -d docset -p -t node_modules/jsdoc-dash-template &&
-tar -cvzf docset/Trimethyl.tgz docset/Trimethyl.docset &&
-rm -rf docset/Trimethyl.docset &&
-echo "<entry><version>$(./version.sh)</version><url>https://github.com/trimethyl/trimethyl/raw/master/docset/Trimethyl.tgz?raw=1</url></entry>" > docset/Trimethyl.xml &&
+# Generate docset for Kapeli Dash
+jsdoc -c jsdoc.conf.json -d docset -p -t ./node_modules/jsdoc-dash-template &&
+tar -cvzf ./docset/Trimethyl.tgz ./docset/Trimethyl.docset &&
+rm -rf ./docset/Trimethyl.docset &&
+echo "<entry><version>$(./version.sh)</version><url>https://github.com/trimethyl/trimethyl/raw/master/docset/Trimethyl.tgz?raw=1</url></entry>" > ./docset/Trimethyl.xml &&
 
+# Move to gh-pages and push forced
 cd /tmp/trimethyl-gh-pages &&
 git init && git checkout -b gh-pages &&
 git add -A &&
