df99a1e729b82b1de51789d2aa3a5be82ccec305
Flavio De Stefano <flavio.destefano@caffeina.com>
2016-10-26 17:37:05 +0200

    No verbose for tar command

--- gendoc.sh
+++ gendoc.sh
@@ -11,7 +11,7 @@ jsdoc -c jsdoc.conf.json -d /tmp/trimethyl-gh-pages &&
 
 # Generate docset for Kapeli Dash
 jsdoc -c jsdoc.conf.json -d docset -p -t ./node_modules/jsdoc-dash-template &&
-tar -cvzf ./docset/Trimethyl.tgz ./docset/Trimethyl.docset &&
+tar -czf ./docset/Trimethyl.tgz ./docset/Trimethyl.docset &&
 rm -rf ./docset/Trimethyl.docset &&
 echo "<entry><version>$(./version.sh)</version><url>https://github.com/trimethyl/trimethyl/raw/master/docset/Trimethyl.tgz?raw=1</url></entry>" > ./docset/Trimethyl.xml &&
 
