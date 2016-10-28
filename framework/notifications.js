bdcd4bab221d9acd7d9541cb69bd0a1e8b9aef8d
Flavio De Stefano <flavio.destefano@caffeina.com>
2016-10-28 17:11:16 +0200

    Refactored Notifications

--- framework/notifications.js
+++ framework/notifications.js
@@ -5,13 +5,15 @@
 
 /**
  * @property config
- * @property {Boolean} 	[config.autoReset=true] 	Check if auto-reset the badge when app is open.
- * @property {String} 	[config.driver="http"] 		The driver to use.
+ * @property {String} 	[config.driver="http"] 					The driver to use.
+ * @property {Boolean} 	[config.autoReset=true] 				If true, set the badge count to 0 when you open/resume the application.
+ * @property {String}	[config.androidModule="ti.goosh"]	The name of the Android module to load.
  * @type {Object}
  */
 exports.config = _.extend({
-	autoReset: true,
 	driver: 'http',
+	autoReset: true,
+	androidModule: 'ti.goosh'
 }, Alloy.CFG.T ? Alloy.CFG.T.notifications : {});
 
 var Event = require('T/event');
@@ -19,26 +21,36 @@ var Util = require('T/util');
 var Router = require('T/router');
 var Q = require('T/ext/q');
 
-var cachedDeviceToken = null;
+// Because Ti.Goosh has the same syntax of Ti.Network, 
+// we can just assign the module and use it in the same way
 
 /**
- * @property onReceived
- * A callback called when a notification is received
+ * The module used for push notifications
+ * @type {Object}
  */
-exports.onReceived = function(e) {
-	Ti.API.info("Notifications: Received", e);
-};
+exports.PushModule = null;
 
-var interactiveCategories = [];
-var interactiveCategoriesCallbacks = {};
+/**
+ * The options for the registerForPushNotifications method of the push module
+ * @type {Object}
+ */
+exports.PushModuleOpt = {};
 
-var INTERACTIVE_NOTIFICATIONS_CAPABLE = (OS_IOS && _.isFunction(Ti.App.iOS.createUserNotificationAction));
-
-function validateToken(token) {
-	return token != null && token != "undefined" && token != "null" && token.length >= 32;
+if (OS_IOS) {
+	exports.PushModule = Ti.Network;
+	exports.PushModuleOpt.types = [ Ti.Network.NOTIFICATION_TYPE_BADGE, Ti.Network.NOTIFICATION_TYPE_ALERT, Ti.Network.NOTIFICATION_TYPE_SOUND ];
+} else if (OS_ANDROID) {
+	exports.PushModule = require(exports.config.androidModule);
+} else {
+	throw new Error("Notifications: unsupported platform");
 }
 
-function onNotificationReceived(e) {
+// The listeners for all received notification
+exports.PushModuleOpt.callback = function(e) {
+	
+	// We must be sure that the processed payload is the same on every platform,
+	// for this reason we have to parse the JSON as described
+	// in Ti.Goosh documentation: https://github.com/caffeinalab/ti.goosh
 	if (OS_ANDROID) {
 		if (_.isString(e.data)) {
 			try {
@@ -49,15 +61,45 @@ function onNotificationReceived(e) {
 		}
 	}
 
-	// Auto-reset the badge
+	// Auto-reset the badge when a notification is received
+	// We are sure the the app is in foreground, otherwise this function is never called on background/killed state.
 	if (exports.config.autoReset === true) {
 		exports.resetBadge();
 	}
 
-	if (_.isFunction(exports.onReceived)) exports.onReceived(e);
-	Event.trigger('notifications.received', e);
-}
+	// Call the local module onReceived method
+	if (_.isFunction(exports.onReceived)) {
+		exports.onReceived(e);
+	}
 
+	// And trigger a global event with the payload
+	exports.trigger('received', e);
+};
+
+var interactiveCategories = [];
+var interactiveCategoriesCallbacks = {};
+
+var INTERACTIVE_NOTIFICATIONS_CAPABLE = (OS_IOS && _.isFunction(Ti.App.iOS.createUserNotificationAction));
+
+/**
+ * Validate a device token using empiric methods.
+ * It only checks the the argument is a string and its length is enough for a valid token.
+ * @param  {String} 	token		The device token
+ * @return {Boolean}
+ */
+exports.validateToken = function(token) {
+	return token != null && token != "undefined" && token != "null" && token.length >= 32;
+};
+
+/**
+ * @property onReceived
+ * A callback called when a notification is received.
+ * You can override this to handle incoming notifications, or just
+ * listen to the global event "notifications.received"
+ */
+exports.onReceived = function(e) {
+	Ti.API.info("Notifications: Received", e);
+};
 
 /**
  * Load a driver of current module
@@ -71,232 +113,227 @@ exports.loadDriver = function(name) {
 
 /**
  * Attach events to current module
+ * @param  {String}   	name 		Event key
+ * @param  {Function} 	cb 		Callback to invoke
  */
 exports.event = function(name, cb) {
 	Event.on('notifications.' + name, cb);
 };
 
 /**
- * @param  {Function} callback Callback invoked when success occur
+ * Trigger events from current module
+ * @param  {String}   	name 		Event key
+ * @param  {Function} 	cb 		The data
  */
-exports.activate = function(callback) {
-	var defer = Q.defer();
-
-	if (cachedDeviceToken != null) {
-		Ti.API.warn("Notifications: multiple call to activate, will return cached remote device UDID");
-		defer.resolve(cachedDeviceToken);
-		return defer.promise;
-	}
-
-	if (INTERACTIVE_NOTIFICATIONS_CAPABLE) {
-
-		var userNotificationsCallback = function(settings) {
-			Ti.App.iOS.removeEventListener('usernotificationsettings', userNotificationsCallback);
-
-			if (_.isEmpty(settings.types)) {
-				return defer.reject({
-					disabled: true
-				});
-			}
-
-			Ti.Network.registerForPushNotifications({
-				callback: onNotificationReceived,
-				success: function(e) {
-					if ( ! validateToken(e.deviceToken)) {
-						Ti.API.error('Notifications: Retrieve device token failed');
-						return defer.reject({
-							invalidToken: true
-						});
-					}
-
-					// Set a flag to prevent that future calls to registerForPush will trigger multiple notifications
-					cachedDeviceToken = e.deviceToken;
-
-					defer.resolve(e.deviceToken);
-					Event.trigger('notifications.activation.success');
-				},
-				error: function(err) {
-					Ti.API.error('Notifications: Retrieve device token failed', err);
-
-					defer.reject(err);
-					Event.trigger('notifications.activation.error', err);
-				}
-			});
-		};
-
-		Ti.App.iOS.addEventListener('usernotificationsettings', userNotificationsCallback);
-		Ti.App.iOS.registerUserNotificationSettings({
-			types: [
-				Ti.Network.NOTIFICATION_TYPE_BADGE,
-				Ti.Network.NOTIFICATION_TYPE_ALERT,
-				Ti.Network.NOTIFICATION_TYPE_SOUND
-			],
-			categories: interactiveCategories
-		});
-
-	} else {
-
-		var Module = null;
-		var moduleOpt = {};
-
-		if (OS_IOS) {
-
-			Module = Ti.Network;
-			moduleOpt.types = [
-				Ti.Network.NOTIFICATION_TYPE_BADGE,
-				Ti.Network.NOTIFICATION_TYPE_ALERT,
-				Ti.Network.NOTIFICATION_TYPE_SOUND
-			];
-
-		} else if (OS_ANDROID) {
-
-			Module = require('ti.goosh');
-
-		} else return;
-
-		Module.registerForPushNotifications(_.extend(moduleOpt, {
-			callback: onNotificationReceived,
-			success: function(e) {
-				if ( ! validateToken(e.deviceToken)) {
-					Ti.API.error('Notifications: Retrieve device token failed');
-					return defer.reject({
-						invalidToken: true
-					});
-				}
-
-				// Set a flag to prevent that future calls to registerForPush will trigger multiple notifications
-				cachedDeviceToken = e.deviceToken;
-
-				defer.resolve(e.deviceToken);
-				Event.trigger('notifications.activation.success');
-			},
-			error: function(err) {
-				Ti.API.error('Notifications: Retrieve device token failed', err);
-
-				defer.reject(err);
-				Event.trigger('notifications.activation.error', err);
-			},
-		}));
-	}
-
-	return defer.promise;
+exports.trigger = function(name, data) {
+	Event.trigger('notifications.' + name, data);
 };
 
+/**
+ * Activate the notifications without a subscription to a service.
+ * You can use this method to just ask to the user the permissions.
+ * @param  {Object}		[opt]
+ * @param  {Function}	[opt.success]	Callback to invoke on success
+ * @return {Q.Promise}	[opt.error]		Callback to invoke on error
+ */
+exports.activate = function(opt) {
+	opt = _.defaults(opt || {}, {
+		success: Alloy.Globals.noop,
+		error: Alloy.Globals.noop
+	});
+
+	return Q.promise(function(_resolve, _reject) {
+
+		var resolve = function(e) {
+			Ti.API.debug('Notification: activation success', e);
+			exports.trigger('activation.success', e);
+			opt.success(e);
+			_resolve(e);
+		};
+
+		var reject = function(e) {
+			Ti.API.error('Notification: activation error', e);
+			exports.trigger('activation.error', e);
+			opt.error(e);
+			_reject(e);
+		};
+
+		var registerForPushNotifications = function() {
+			if (exports.PushModule.remoteNotificationsEnabled) {
+				resolve(exports.PushModule.remoteDeviceUUID);		
+			} else {
+				exports.PushModule.registerForPushNotifications(_.extend(exports.PushModuleOpt, {
+					success: function(e) {
+						resolve(e.deviceToken);
+					},
+					error: reject
+				}));
+			}
+		};
+
+		if (INTERACTIVE_NOTIFICATIONS_CAPABLE) {
+
+			var userNotificationsCallback = function(settings) {
+				// Instantly remove this listener
+				Ti.App.iOS.removeEventListener('usernotificationsettings', userNotificationsCallback);
+
+				if (_.isEmpty(settings.types)) {
+					// The user has not accepted the notifications permissions
+					return reject({ disabled: true });
+				}
+
+				registerForPushNotifications();
+			};
+
+			Ti.App.iOS.addEventListener('usernotificationsettings', userNotificationsCallback);
+			Ti.App.iOS.registerUserNotificationSettings({
+				types: [ 
+				Ti.App.iOS.USER_NOTIFICATION_TYPE_BADGE, 
+				Ti.App.iOS.USER_NOTIFICATION_TYPE_ALERT, 
+				Ti.App.iOS.USER_NOTIFICATION_TYPE_SOUND 
+				],
+				categories: interactiveCategories
+			});
+
+		} else {
+			registerForPushNotifications();
+		}
+
+	});
+};
 
 /**
  * Deactivate completely the notifications
+ * Per Apple's documentation, it is rarely necessary to call this method.
+ * For example, calling this method would be required if a new version of your application no longer supports push notifications.
  */
 exports.deactivate = function() {
-	if (OS_IOS) {
-		Module = Ti.Network;
-	} else if (OS_ANDROID) {
-		Module = require("ti.goosh");
-	} else return;
-
-	Module.unregisterForPushNotifications();
+	exports.PushModule.unregisterForPushNotifications();
 };
 
 /**
- * Subscribe for that channel
- * @param {String} channel 	Channel name
- * @param {Object} data 		Additional data
- * @param {Object} [opt={}] 	Additional options
+ * The main method: It activate the notifications (asking to the user the permissions), and sends the device token to your driver.
+ * @param {String} 	[channel] 		Channel name
+ * @param {Object} 	[data] 			Additional data to pass to the driver
+ * @param {Object} 	[opt] 			Additional options to pass to the driver
+ * @return {Q.Promise}
  */
 exports.subscribe = function(channel, data, opt) {
-	var defer = Q.defer();
-
-	if (!Ti.Network.online) {
-		defer.reject({
-			offline: true
-		});
-		return defer.promise;
-	}
-
-	exports.activate()
-	.then(function(deviceToken) {
-
-		exports.loadDriver(exports.config.driver).subscribe(_.extend({}, opt, {
-			deviceToken: deviceToken,
-			channel: channel,
-			data: data,
-			success: function(response) {
-				Event.trigger('notifications.subscription.success', { channel: channel });
-				Ti.API.debug('Notifications: Subscription to channel <' + (channel || 'default') + '> succeded', response);
-
-				defer.resolve(response);
-			},
-			error: function(err) {
-				Event.trigger('notifications.subscription.error', err);
-				Ti.API.error('Notifications: Subscription failed to channel <' + (channel || 'default') + '>', err);
-
-				defer.reject(err);
-			}
-		}));
-
-	})
-	.fail(defer.reject);
-
-	return defer.promise;
-};
-
-
-/**
- * Unsubscribe for that channel
- * @param {String} channel 	Channel name
- * @param {Object} data 		Additional data
- */
-exports.unsubscribe = function(channel, data) {
-	var defer = Q.defer();
-
-	if (!Ti.Network.online) {
-		defer.reject({
-			offline: true
-		});
-		return defer.promise;
-	}
-
-	exports.loadDriver(exports.config.driver).unsubscribe({
-		deviceToken: exports.getRemoteDeviceUUID(),
-		channel: channel,
-		data: data,
-		success: function(response) {
-			Event.trigger('notifications.unsubscription.error', { channel: channel });
-			Ti.API.debug('Notifications: Unsubscription to channel <' + (channel || 'default') + '> succeded', response);
-
-			defer.resolve(response);
-		},
-		error: function(err) {
-			Event.trigger('notifications.unsubscription.error', err);
-			Ti.API.error('Notifications: Unsubscription failed to channel <' + (channel || 'default') + '>', err);
-
-			defer.reject(err);
-		}
+	opt = _.defaults(opt || {}, {
+		success: Alloy.Globals.noop,
+		error: Alloy.Globals.noop
 	});
 
-	return defer.promise;
+	return Q.promise(function(_resolve, _reject) {
+
+		var resolve = function(e) {
+			Ti.API.debug('Notification: subscription success', e);
+			exports.trigger('subscription.success', e);
+			opt.success(e);
+			_resolve(e);
+		};
+
+		var reject = function(e) {
+			Ti.API.error('Notification: subscription error', e);
+			exports.trigger('subscription.error', e);
+			opt.error(e);
+			_reject(e);
+		};
+
+		if (false == Ti.Network.online) {
+			return reject({
+				offline: true
+			});
+		}
+
+		exports.activate()
+		.then(function(deviceToken) {
+
+			var driver = exports.loadDriver(exports.config.driver);
+
+			driver.subscribe(_.extend({}, opt, {
+				deviceToken: deviceToken,
+				channel: channel,
+				data: data,
+				success: resolve,
+				error: reject
+			}));
+
+		})
+		.fail(reject);
+
+	});
 };
 
-
 /**
- * Set the App badge value
- * @param {Number} x
+ * Unsubscribe from a channel using the specified driver.
+ * @param {String} 	[channel] 		Channel name
+ * @param {Object} 	[data] 			Additional data to pass to the driver
+ * @param {Object} 	[opt] 			Additional options to pass to the driver
+ * @return {Q.Promise}
  */
-exports.setBadge = function(x) {
-	var Module = OS_IOS ? Ti.UI.iPhone : require('ti.goosh');
-	Module.setAppBadge(Math.max(x,0));
+exports.unsubscribe = function(channel, data, opt) {
+	opt = _.defaults(opt || {}, {
+		success: Alloy.Globals.noop,
+		error: Alloy.Globals.noop
+	});
+
+	return Q.promise(function(_resolve, _reject) {
+
+		var resolve = function(e) {
+			Ti.API.debug('Notification: subscription success', e);
+			exports.trigger('unsubscription.success', e);
+			opt.success(e);
+			_resolve(e);
+		};
+
+		var reject = function(e) {
+			Ti.API.error('Notification: subscription error', e);
+			exports.trigger('unsubscription.error', e);
+			opt.error(e);
+			_reject(e);
+		};
+
+		if (false == Ti.Network.online) {
+			return reject({
+				offline: true
+			});
+		}
+
+		var driver = exports.loadDriver(exports.config.driver);
+		var device_token = exports.getRemoteDeviceUUID();
+
+		driver.unsubscribe(_.extend({}, opt, {
+			deviceToken: device_token,
+			channel: channel,
+			data: data,
+			success: resolve,
+			error: reject
+		}));
+
+	});
 };
 
 /**
- * Get the App badge value
+ * Set the application badge number
+ * @param {Number} 	value
+ */
+exports.setBadge = function(value) {
+	var proxy = OS_IOS ? Ti.UI.iOS : exports.PushModule;
+	proxy.setAppBadge(Math.max(value, 0));
+};
+
+/**
+ * Get the application badge number
  * @return {Number}
  */
 exports.getBadge = function() {
-	var Module = OS_IOS ? Ti.UI.iPhone : require('ti.goosh');
-	return Module.getAppBadge();
+	var proxy = OS_IOS ? Ti.UI.iOS : exports.PushModule;
+	return proxy.getAppBadge();
 };
 
 /**
- * Reset to 0 the badge
+ * Set the application badge to zero
  */
 exports.resetBadge = function() {
 	exports.setBadge(0);
@@ -304,10 +341,10 @@ exports.resetBadge = function() {
 
 /**
  * Increment the badge app
- * @param  {Number} i The value to increment
+ * @param {Number} value 	The value to increment
  */
-exports.incBadge = function(i) {
-	exports.setBadge(exports.getBadge() + i);
+exports.incBadge = function(value) {
+	exports.setBadge(exports.getBadge() + value);
 };
 
 /**
@@ -316,18 +353,16 @@ exports.incBadge = function(i) {
  * @return {String}
  */
 exports.getRemoteDeviceUUID = function() {
-	var Module = OS_IOS ? Ti.Network : require('ti.goosh');
-	return Module.remoteDeviceUUID;
+	return exports.PushModule.remoteDeviceUUID;
 };
 
 /**
- * Check if the remote notifications has been registered once
+ * Check if the remote notifications has been registered once.
  * Use this method at startup in conjunction with `activate()`
  * @return {Boolean} [description]
  */
 exports.isRemoteNotificationsEnabled = function() {
-	var Module = OS_IOS ? Ti.Network : require('ti.goosh');
-	return Module.remoteNotificationsEnabled;
+	return exports.PushModule.remoteNotificationsEnabled;
 };
 
 
@@ -335,6 +370,7 @@ exports.isRemoteNotificationsEnabled = function() {
 // Interactive notifications //
 ///////////////////////////////
 
+// Create a UserNotificationAction Object based on a dictionary.
 function createIntNotifAction(opt) {
 	if (opt.id == null) throw new Error('Notifications: interactive notifications must have and ID');
 	if (opt.title == null) throw new Error('Notifications: interactive notifications must have a title');
@@ -350,11 +386,18 @@ function createIntNotifAction(opt) {
 
 /**
  * @param {String}   id       	The ID of the category. It must be unique.
- * @param {Array}    dict     	An array of actions, with `id, title` (required) and `openApplication, destructive, authenticationRequired` (optional)
+ * @param {Array}    dict     	An array of actions. Each action must have defined:
+ * `id` (required), the ID of this action
+ * `title` (required) the title to show
+ * `callback` (required) the callback that handle the user click
+ * `openApplication`, `destructive`, `authenticationRequired` are optionals.
  * @param {Function} callback 	The callback to invoke
  */
-exports.addInteractiveNotificationCategory = function(id, dict, callback) {
-	if (!INTERACTIVE_NOTIFICATIONS_CAPABLE) return;
+exports.addInteractiveNotificationCategory = function(id, dict) {
+	if (false == INTERACTIVE_NOTIFICATIONS_CAPABLE) {
+		Ti.API.error("Notifications: unable to create an interactive notification category, not supported");
+		return;
+	}
 
 	var category = Ti.App.iOS.createUserNotificationCategory({
 		identifier: id,
@@ -363,9 +406,8 @@ exports.addInteractiveNotificationCategory = function(id, dict, callback) {
 
 	// Add in the interactiveCategories array to register in the activate method
 	interactiveCategories.push(category);
-	interactiveCategoriesCallbacks[id] = callback;
+	interactiveCategoriesCallbacks[ id ] = dict.callback;
 };
-
 
 //////////
 // Init //
@@ -373,7 +415,7 @@ exports.addInteractiveNotificationCategory = function(id, dict, callback) {
 
 if (INTERACTIVE_NOTIFICATIONS_CAPABLE) {
 	Ti.App.iOS.addEventListener('remotenotificationaction', function(e) {
-		var func = interactiveCategoriesCallbacks[e.category];
+		var func = interactiveCategoriesCallbacks[ e.category ];
 		if (_.isFunction(func)) {
 			func(e);
 		} else {
