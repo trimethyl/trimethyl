af5f0bd237db0cc498be35fa5dce13a3bad1fbee
Ani Sinanaj <ani.sinanaj@caffeina.com>
2017-05-17 11:02:28 +0200

    Updated prop

--- framework/prop.js
+++ framework/prop.js
@@ -5,11 +5,13 @@
 
 /**
  * * @property {String} [config.encryptionKey=null] Secret to use in keychain encryption.
+ * * @property {Bool} [config.allowSync=true] Whether to sync data with iCloud
+ * * @property {String} [config.method="dynamic"] What method to use for the key, "dynamic" can be synced, "random" cannot
  */
 exports.config = _.extend({
 	encryptionKey: null,
 	allowSync: true,
-	method: "static"
+	method: "dynamic"
 }, Alloy.CFG.T ? Alloy.CFG.T.prop : {});
 
 var MODULE_NAME = 'prop';
@@ -22,23 +24,16 @@ if (Securely == null) {
 	Ti.API.warn(MODULE_NAME + ": you are not including the security module, your auth storage is not secure");
 	module.exports = Ti.App.Properties;
 } else {
-	module.exports = initWithStaticKey();
+	if (exports.config.method == "random") module.exports = initWithDynamicKey();
+	else module.exports = initWithStaticAndDynamicKeys();
 }
 
 /**
  * Private functions
  */
 
-function initWithStaticKey() {
-	return Securely.createProperties({
-		secret: exports.config.encriptionKey,
-		allowSync: exports.config.allowSync,
-		debug: !ENV_PRODUCTION,
-	});
-}
-
 function initWithDynamicKey() {
-	var key = scrambleKey(Ti.App.installId);
+	var key = scrambleKey(exports.config.encryptionKey);
 
 	return Securely.createProperties({
 		secret: key,
@@ -48,27 +43,29 @@ function initWithDynamicKey() {
 }
 
 function initWithStaticAndDynamicKeys() {
+	var stringCrypto = Securely.createStringCrypto();
+	var id = stringCrypto.sha256(Ti.App.id);
 	var encKey = null;
 	var starter = Securely.createProperties({
-		identifier: Ti.App.id + ".starter",
+		identifier: "_" + Ti.App.id + ".starter",
 		secret: exports.config.encryptionKey,
 		allowSync: exports.config.allowSync,
 		debug: !ENV_PRODUCTION
 	});
 
 	// Saving dynamic key in keychain with key "Ti.App.id" and secret the static `encryptionKey`
-	if (!starter.hasProperty(Ti.App.id)) {
-		var key = scrambleKey(Securely.generateRandomKey());
-		starter.setString(Ti.App.id, key);
+	if (!starter.hasProperty(id)) {
+		var key = _.isFunction($.generator) ? $.generator(key) : Securely.generateRandomKey();
+		starter.setString(id, key);
 		encKey = key;
 	} else {
-		encKey = starter.getString(Ti.App.id);
+		encKey = starter.getString(id);
 	}
 
 	return Securely.createProperties({
-		identifier: Ti.App.id + ".data",
+		identifier: "_" + Ti.App.id + ".data",
 		secret: encKey,
-		allowSync: false,
+		allowSync: exports.config.allowSync,
 		debug: !ENV_PRODUCTION
 	});
 }
@@ -76,16 +73,32 @@ function initWithStaticAndDynamicKeys() {
 function scrambleKey(key) {
 	if (_.isFunction($.generator)) return $.generator(key);
 	var stringCrypto = Securely.createStringCrypto();
-	key = key + Ti.App.installId + key.split('').reverse().join('');
+	key = key + installId();
 	key = stringCrypto.AESEncrypt(exports.config.encryptionKey, key);
 	return key;
 }
 
+function installId() {
+	if (OS_IOS) return Ti.App.installId;
+
+	var iid = Ti.Filesystem.getFile(Util.getAppDataDirectory(), 'iid');
+	if (iid.exists()) {
+		return iid.read().text;
+	}
+
+	var key = Securely.generateRandomKey();
+	iid.open(Titanium.Filesystem.MODE_WRITE);
+	iid.write(key, false);
+
+	return key;
+}
 
 /**
  * Public functions
  */
 
-$.setGenerator = function(fn) {
+module.exports.setGenerator = function(fn) {
 	$.generator = _.isFunction(fn) ? fn : null;
 };
+
+module.exports.installId = installId();
