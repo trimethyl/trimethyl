278663d61107d4163c1a623ad560f9399ef6d7ed
Flavio De Stefano <destefano.flavio@gmail.com>
2017-05-19 18:42:31 +0200

    Refactor Prop

--- framework/prop.js
+++ framework/prop.js
@@ -6,17 +6,20 @@
 /**
  * * @property {String} [config.encryptionKey=null] Secret to use in keychain encryption.
  * * @property {Bool} [config.allowSync=true] Whether to sync data with iCloud
+ * * @property {String} [config.accessGroup=null] The access group identifier to share data along apps
  */
 exports.config = _.extend({
 	encryptionKey: null,
-	allowSync: true
+	allowSync: true,
+	accessGroup: null
 }, Alloy.CFG.T ? Alloy.CFG.T.prop : {});
 
 var MODULE_NAME = 'prop';
 
 var Util = require('T/util');
 var Securely = Util.requireOrNull('bencoding.securely');
-exports.generator = null;
+
+var generator = null;
 
 if (Securely == null) {
 	Ti.API.warn(MODULE_NAME + ": you are not including the security module, your auth storage is not secure");
@@ -31,18 +34,21 @@ if (Securely == null) {
 
 function initWithStaticAndDynamicKeys() {
 	var stringCrypto = Securely.createStringCrypto();
-	var id = stringCrypto.sha256(Ti.App.id);
+	var id = Ti.Utils.sha256(Ti.App.id);
 	var encKey = null;
+	
 	var starter = Securely.createProperties({
-		identifier: "_" + Ti.App.id + ".starter",
+		identifier: Ti.App.id + '.starter',
 		secret: exports.config.encryptionKey,
 		allowSync: exports.config.allowSync,
 		debug: !ENV_PRODUCTION
 	});
 
-	// Saving dynamic key in keychain with key "Ti.App.id" and secret the static `encryptionKey`
+	// Saving dynamic key in keychain with key 'Ti.App.id' and secret the static `encryptionKey`
 	if (!starter.hasProperty(id)) {
-		var key = _.isFunction(exports.generator) ? exports.generator(key) : Securely.generateRandomKey();
+		var key = _.isFunction(generator) ? 
+			exports.generator(key) : 
+			Securely.generateRandomKey();
 		starter.setString(id, key);
 		encKey = key;
 	} else {
@@ -50,21 +56,12 @@ function initWithStaticAndDynamicKeys() {
 	}
 
 	return Securely.createProperties({
-		identifier: "_" + Ti.App.id + ".data",
+		identifier: Ti.App.id,
 		secret: encKey,
+		accessGroup: exports.config.accessGroup,
 		allowSync: exports.config.allowSync,
 		debug: !ENV_PRODUCTION
 	});
-}
-
-function installId() {
-	if (OS_IOS) return Ti.App.installId;
-	if (Ti.App.Properties.hasProperty("uuid")) return Ti.App.Properties.getString("uuid");
-
-	var key = Securely.generateRandomKey();
-	Ti.App.Properties.setString("uuid", key);
-
-	return key;
 }
 
 /**
@@ -74,5 +71,3 @@ function installId() {
 exports.setGenerator = function(fn) {
 	exports.generator = _.isFunction(fn) ? fn : null;
 };
-
-exports.installId = installId();
