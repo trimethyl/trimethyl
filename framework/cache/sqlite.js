85b2145ca99458d87018bdd08d620d529fc880dd
Flavio De Stefano <destefano.flavio@gmail.com>
2017-05-29 11:38:50 +0200

    Refactor Cache and added Cache/Prop

--- framework/cache/sqlite.js
+++ framework/cache/sqlite.js
@@ -1,33 +1,20 @@
 /**
- * @module  cache/database
+ * @module  cache/sqlite
  * @author  Flavio De Stefano <flavio.destefano@caffeinalab.com>
  */
+
+var MODULE_NAME = 'cache.sqlite';
 
 var SQLite = require('T/sqlite');
 var Util = require('T/util');
 
-var DIR = Ti.Filesystem.applicationCacheDirectory + 'database';
+var DB_NAME = 'trimethyl';
 var TABLE = 'cachedb';
 
-var purged = false;
-
 function getDatabase() {
-	Ti.Filesystem.getFile(Ti.Filesystem.applicationCacheDirectory).createDirectory();
-	Ti.Filesystem.getFile(DIR).createDirectory();
-
-	var DB = new SQLite('app');
-	DB.run('CREATE TABLE IF NOT EXISTS ' + TABLE + ' (hash TEXT PRIMARY KEY, expire INTEGER, info TEXT)');
-
-	// Delete oldest keys
-	if (purged == false) {
-		purged = true;
-
-		DB.list('SELECT hash FROM ' + TABLE + ' WHERE expire < ' + Util.now()).forEach(function(h) {
-			Ti.API.trace('Cache: removing expired key ' + h);
-			exports.remove(h);
-		});
-	}
-
+	var DB = new SQLite(DB_NAME);
+	DB.run('CREATE TABLE IF NOT EXISTS ' + TABLE + ' (hash TEXT PRIMARY KEY, expire INTEGER, value BLOB)');
+	DB.run('DELETE FROM ' + TABLE + ' WHERE expire < ?', Util.now());
 	return DB;
 }
 
@@ -39,20 +26,10 @@ function getDatabase() {
 exports.get = function(hash) {
 	var DB = getDatabase();
 
-	var row = DB.row('SELECT expire, info FROM ' + TABLE + ' WHERE hash = ? LIMIT 1', hash);
+	var row = DB.row('SELECT * FROM ' + TABLE + ' WHERE hash = ? AND expire >= ? LIMIT 1', hash, Util.now());
 	if (row == null) return null;
 
-	var expire = row.expire << 0;
-	if (Util.now() > expire) return null;
-
-	var file = Ti.Filesystem.getFile(DIR, hash);
-	if (!file.exists()) return null;
-
-	return {
-		value: file.read(),
-		expire: expire,
-		info: Util.parseJSON(row.info)
-	};
+	return row.value;
 };
 
 /**
@@ -60,18 +37,15 @@ exports.get = function(hash) {
  * @param {String} 	hash
  * @param {Object} 	value
  * @param {Number} 	ttl
- * @param {Object} 	info
+ * @return {Boolean}
  */
-exports.set = function(hash, value, ttl, info) {
+exports.set = function(hash, value, ttl) {
 	var DB = getDatabase();
-	
-	info = JSON.stringify(info || {});
-	if (_.isObject(value) || _.isArray(value)) value = JSON.stringify(value);
-
-	var expire = Util.fromNow(ttl || 0);
-
-	DB.run('INSERT OR REPLACE INTO ' + TABLE + ' (hash, expire, info) VALUES (?, ?, ?)', hash, expire, info);
-	Ti.Filesystem.getFile(DIR, hash).write(value);
+	DB.run('INSERT OR REPLACE INTO ' + TABLE + ' (hash, expire, value) VALUES (?, ?, ?)', 
+		hash,
+		Util.fromNow(ttl || 0), 
+		value
+	);
 };
 
 
@@ -81,9 +55,7 @@ exports.set = function(hash, value, ttl, info) {
  */
 exports.remove = function(hash) {
 	var DB = getDatabase();
-	
 	DB.run('DELETE FROM ' + TABLE + ' WHERE hash = ?', hash);
-	Ti.Filesystem.getFile(DIR, hash).deleteFile();
 };
 
 /**
@@ -91,15 +63,13 @@ exports.remove = function(hash) {
  */
 exports.purge = function() {
 	var DB = getDatabase();
-	
-	DB.run('DELETE FROM ' + TABLE + ' WHERE 1');
-	Ti.Filesystem.getFile(DIR).deleteDirectory(true);
-	Ti.Filesystem.getFile(DIR).createDirectory();
+	DB.run('TRUNCATE TABLE ' + TABLE);
 };
 
 /**
  * @return {Number}
  */
 exports.getSize = function() {
-	return require('T/filesystem').getSize(DIR);
+	var DIR = Util.getDatabaseDirectoryName();
+	return require('T/filesystem').getSize(DIR + '/' + DB_NAME + '.sql');
 };
