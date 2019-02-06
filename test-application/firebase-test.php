a838f3f4f5223c795e4b83daade093e6f67345ca
Andrea Jonus <andrea.jonus@caffeina.com>
2019-02-06 10:03:59 +0100

    Reformatted FCM test

--- test-application/firebase-test.php
+++ test-application/firebase-test.php
@@ -1,25 +1,25 @@
 <?php $url = 'https://fcm.googleapis.com/fcm/send';
 
 	$fields = array (
-			'to' => "<DEVICE TOKEN>",
-			// 'to' => "/topics/test",
-			'notification' => array (
-				"title" => "TiFirebaseMessaging",
-				"body" => "Message received 📱😂",
-				// "timestamp"=>date('Y-m-d G:i:s'),
-			),
-			'data' => array(
-				"title" => "test",
-				"test1" => "value1",
-				"test2" => "value2",
-				"test3" => "value3",
-				"channelId" => "default"	// or a different channel
-			)
+		'to' => "ecrktQG_prA:APA91bH_hf1VjEcoDsNZTwpCb4NJk6ioG8B-sEdbFKHRGN7RGpjDTGEnzR_7e1Fw0Yucb7_tkqcSHD47s_xYIYdicLqgPDsYglZvAS_WodgvAp9xSHHlekUxwC_rzotKlxgBoXChSk1u",
+		// 'to' => "/topics/test",
+		'notification' => array (
+			"title" => "TiFirebaseMessaging",
+			"body" => "Message received 📱😂",
+			// "timestamp"=>date('Y-m-d G:i:s'),
+		),
+		'data' => array(
+			"title" => "test",
+			"test1" => "value1",
+			"test2" => "value2",
+			"test3" => "value3",
+			"channelId" => "default"	// or a different channel
+		)
 	);
 
 	$headers = array (
-			'Authorization: key=<SERVER KEY>',
-			'Content-Type: application/json'
+		'Authorization: key=AAAAyBiz4Ws:APA91bEEDDtYHTT0KV-9Kj-ohybgsBfqY6As98gZrskPh0Bo4qbiIq_rX53U3xMunpuMVGQTHAt9-nINe2mhNxK8pNmIIFs97QvDVSzvxfvVrh44JJDRW8IoayKmezNx9_E62hJd1beF',
+		'Content-Type: application/json'
 	);
 
 	$ch = curl_init ();
