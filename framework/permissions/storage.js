3c5f1eedd680c8094a6ef94d1a496adf61c94f34
Andrea Jonus <andrea.jonus@caffeina.com>
2019-02-15 16:49:39 +0100

    Fixed storage permission request

--- framework/permissions/storage.js
+++ framework/permissions/storage.js
@@ -9,17 +9,16 @@ var MODULE_NAME = 'permissions.storage';
 var _ = require('alloy/underscore')._;
 var Q = require('T/ext/q');
 
-exports.request = function(success, error) {
+function requestPermissions(permissions, success, error) {
 	return Q.promise(function(_resolve, _reject) {
-
-		var resolve = function() { 
+		var resolve = function() {
 			if (success != null) success.apply(null, arguments);
-			_resolve.apply(null, arguments); 
+			_resolve.apply(null, arguments);
 		};
-		
-		var reject = function() { 
+
+		var reject = function() {
 			if (error != null) error.apply(null, arguments);
-			_reject.apply(null, arguments); 
+			_reject.apply(null, arguments);
 		};
 
 		function requestHandler(e) {
@@ -27,23 +26,48 @@ exports.request = function(success, error) {
 				resolve();
 			} else {
 				Ti.API.error(MODULE_NAME + ': Error while requesting Storage permissions - ' + e.error);
-				reject({ 
-					message: L('error_storage_permissions', 'Missing storage permissions') 
+				reject({
+					message: L('error_storage_permissions', 'Missing storage permissions')
 				});
 			}
 		}
 
-		if (
-			false === _.isFunction(Ti.Filesystem.hasStoragePermissions) || 
-			false === _.isFunction(Ti.Filesystem.requestStoragePermissions)
-		) {
+		if (_.isEmpty(permissions)) {
+			Ti.API.error(MODULE_NAME + ': Error while requesting Storage permissions - No permission defined');
+			reject({
+				message: 'No permission defined',
+			});
+		}
+
+		if (!OS_ANDROID) {
 			return resolve();
 		}
 
-		if (Ti.Filesystem.hasStoragePermissions() !== true) {
-			return Ti.Filesystem.requestStoragePermissions(requestHandler);
-		}
+		return permissions.map(function(permission) {
+			if (Ti.Android.hasPermission(permission)) return Q.resolve();
 
-		resolve();
+			return Ti.Android.requestPermissions(permission, requestHandler);
+		})
+		.reduce(Q.when, Q())
+		.then(resolve);
 	});
+}
+
+exports.request = function(success, error) {
+	return requestPermissions([
+		"android.permission.READ_EXTERNAL_STORAGE",
+		"android.permission.WRITE_EXTERNAL_STORAGE"
+	], success, error);
+};
+
+exports.requestRead = function(success, error) {
+	return requestPermissions([
+		"android.permission.READ_EXTERNAL_STORAGE",
+	], success, error);
+};
+
+exports.requestWrite = function(success, error) {
+	return requestPermissions([
+		"android.permission.WRITE_EXTERNAL_STORAGE"
+	], success, error);
 };
